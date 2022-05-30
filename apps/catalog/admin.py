2183626ede0f46b8ed2348441357a04e45bbd041
Виктор Постников <G.abrie1@yandex.ru>
2022-05-30 14:16:52 +0500

    add models/admin

---
+++ apps/catalog/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+from apps.catalog.models import *
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'slug', 'publish']
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'slug', 'publish']
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'slug', 'publish']
+
+
+class PriceInLine(admin.TabularInline):
+    model = Price
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'slug', 'publish']
+    filter_horizontal = ['colors']
+    inlines = [PriceInLine,]
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'slug', 'publish']
+
+
+@admin.register(PriceType)
+class PriceType(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'slug', 'publish']
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ['product', 'price_type', 'price', 'publish']
---
+++ apps/catalog/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField('title', max_length=255)
+    slug = models.SlugField('slug', max_length=255)
+    publish = models.BooleanField('publish', default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Collection(models.Model):
+    title = models.CharField('title', max_length=255)
+    slug = models.SlugField('slug', max_length=255)
+    publish = models.BooleanField('publish', default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Material(models.Model):
+    title = models.CharField('title', max_length=255)
+    slug = models.SlugField('slug', max_length=255)
+    publish = models.BooleanField('publish', default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Color(models.Model):
+    title = models.CharField('title', max_length=255)
+    slug = models.SlugField('slug', max_length=255)
+    publish = models.BooleanField('publish', default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField('title', max_length=255)
+    slug = models.SlugField('slug', max_length=255)
+    category = models.ForeignKey(
+        Category,
+        related_name='category_products',
+        on_delete=models.CASCADE
+    )
+    collection = models.ForeignKey(
+        Collection,
+        related_name='collection_products',
+        on_delete=models.CASCADE
+    )
+    material = models.ForeignKey(
+        Material,
+        related_name='material_products',
+        on_delete=models.CASCADE
+    )
+    colors = models.ManyToManyField(Color, related_name='colors_products')
+    publish = models.BooleanField('publish', default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PriceType(models.Model):
+    title = models.CharField('title', max_length=255)
+    slug = models.SlugField('slug', max_length=255)
+    publish = models.BooleanField('publish', default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Price(models.Model):
+    product = models.ForeignKey(
+        Product,
+        related_name='product_prices',
+        on_delete=models.CASCADE
+    )
+    price_type = models.ForeignKey(
+        PriceType,
+        related_name='price_type_prices',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    price = models.FloatField('price')
+    publish = models.BooleanField('publish', default=True)
+
+    def __str__(self):
+        return self.product.title
